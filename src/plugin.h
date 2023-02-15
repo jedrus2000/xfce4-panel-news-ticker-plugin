485cd69d8fddb9c13e43d9d47d1beb49a72d72d1
Andrzej Bargański <a.barganski@gmail.com>
2023-02-15 09:59:53 +0100

    testing XFCE4 plugin

--- src/plugin.h
+++ src/plugin.h
@@ -1,18 +1,5 @@
-#ifndef _SAMPLE_PY_PLUGIN_
-#define _SAMPLE_PY_PLUGIN_
 
-#include <gtk/gtk.h>
-#include <libxfce4panel/libxfce4panel.h>
-#include <Python.h>
-
-G_BEGIN_DECLS
-typedef struct
-{
-    XfcePanelPlugin *plugin;
-    PyObject* py_object;
-    void* library_handler;
-    
-} SamplePyPlugin;
-
-G_END_DECLS
+#ifndef __PLUGIN_H__
+#define __PLUGIN_H__
+void constructor(void *);
 #endif
